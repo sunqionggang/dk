7a778670f5f6ba8f03c221bf20f6f6260cc667ec
sunqionggang <913111870@qq.com>
2020-09-28 15:09:25 +0800

    文件上传

---
+++ src/main/java/dockerproj/dkject/filrUpload/FileUploadController.java
@@ -0,0 +1,40 @@
+package dockerproj.dkject.filrUpload;/**
+ * @Author:sqg
+ * @Description
+ * @Date:${Time} ${Date}
+ * @Modified By:
+ **/
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @description: 文件上传和下载
+ * @author: sqg
+ * @create: 2020-09-27 18:03
+ */
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file){
+
+        String type=file.getContentType();
+        String fileName=file.getName();
+
+        //源文件名
+        String originFileName=file.getOriginalFilename();
+        Long size=file.getSize();
+        try {
+            file.transferTo(new File("d:\\"+originFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传成功";
+    }
+}
